de71dc32ce51ffa747f54e27967d1acb774478fb
ivo1996 <ivelin.dimitrov9@gmail.com>
2021-02-02 18:28:48 +0200

    init project

---
+++ src/main/java/com/uni/jobofferprocessor/JobOfferProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.uni.jobofferprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobOfferProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobOfferProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uni/jobofferprocessor/JobOfferProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uni.jobofferprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobOfferProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
